349cce2b9a775b106d2a8eaf1281346750120e7d
arnaudletroter <arnaud.le-troter@univ-amu.fr>
2025-07-28 11:29:13 +0200

    change mask name

--- postprocessing/bias_correction_all_sessions.sh
+++ postprocessing/bias_correction_all_sessions.sh
@@ -5,7 +5,7 @@ for ses in "${sessions[@]}"; do
     postprocessing/T1xT2BiasFieldCorrection.sh \
         -t1 ${path_template}/sub-BaBa21_${ses}_desc-symmetric-sharpen_T1w.nii.gz \
         -t2 ${path_template}/sub-BaBa21_${ses}_desc-symmetric-sharpen_T2w.nii.gz \
-        -b ${path_template}/sub-BaBa21_${ses}_desc-HR-symmetric_desc-brain_mask_probseg.nii.gz \
+        -b ${path_template}/sub-BaBa21_${ses}_desc-symmetric_desc-brain_mask_probseg.nii.gz \
         -k -s 2 \
         -os "_desc-debiased"
 done
